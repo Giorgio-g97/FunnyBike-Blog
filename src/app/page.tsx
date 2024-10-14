d4b8e726b674ac2f9683b4159454659189665131
Giorgio-g97 <giorgio.g97@gmail.com>
2024-10-14 23:10:29 +0200

    Pause at 1:10 video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import style from "./home.module.css";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,8 +6,24 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <h1>Homepage</h1>
+      <div className={style.container}>Homepage</div>
+      <div className={style.textContainer}>
+        <h1>Scopri Foggia in Bicicletta</h1>
+        <h3>Unisciti a noi per esplorare la città su due ruote!</h3>
+        <p>
+          Pedala attraverso le bellezze di Foggia, scopri angoli nascosti e vivi
+          esperienze uniche con altri appassionati. Che tu sia un ciclista
+          esperto o un neofita, le nostre uscite sono pensate per tutti!
+        </p>
+        <div>
+          <h3>
+            🚴‍♀️ <button>Registrati oggi</button> e inizia la tua avventura!
+          </h3>
+          <p>Foggia ti aspetta: pedala, esplora e divertiti!</p>
+        </div>
+      </div>
+      <div className={style.imageContainer}>
+        
       </div>
     </>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Links from "./links/Links";
 import style from "./navbar.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <>
       <div className={style.container}>
-        <div className={style.logo}>Logo</div>
+        <Link href="/" className={style.logo}>
+          <Image src="/logo.png" width={80} height={80} alt="logo-funny-bike" />
+        </Link>
         <Links />
       </div>
     </>
