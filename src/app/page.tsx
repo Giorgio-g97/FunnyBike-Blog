afb10fcceb830255ef4fb74776d31ef881cea207
Giorgio-g97 <giorgio.g97@gmail.com>
2024-10-15 00:50:46 +0200

    Finito (più o meno) Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,37 @@ export default function Home() {
 
   return (
     <>
-      <div className={style.container}>Homepage</div>
-      <div className={style.textContainer}>
-        <h1>Scopri Foggia in Bicicletta</h1>
-        <h3>Unisciti a noi per esplorare la città su due ruote!</h3>
-        <p>
-          Pedala attraverso le bellezze di Foggia, scopri angoli nascosti e vivi
-          esperienze uniche con altri appassionati. Che tu sia un ciclista
-          esperto o un neofita, le nostre uscite sono pensate per tutti!
-        </p>
-        <div>
-          <h3>
-            🚴‍♀️ <button>Registrati oggi</button> e inizia la tua avventura!
-          </h3>
-          <p>Foggia ti aspetta: pedala, esplora e divertiti!</p>
+      <div className={style.container}>
+        <div className={style.textContainer}>
+          <div className={style.headerContainer}>
+            <h1>
+              Scopri <span>Foggia</span> in Bicicletta
+            </h1>
+          </div>
+          <div className={style.description}>
+            <h3 className={style.headerDescription}>
+              Unisciti a noi per esplorare la città su due ruote!
+            </h3>
+            <p className={style.pDescription}>
+              Pedala attraverso le bellezze di Foggia, scopri angoli nascosti e
+              vivi esperienze uniche con altri appassionati. Che tu sia un
+              ciclista esperto o un neofita, le nostre uscite sono pensate per
+              tutti!
+            </p>
+            <div className={style.containerBtn}>
+              <h3 className={style.headerBtn}>
+                <button className={style.button}>Registrati oggi</button> e
+                inizia la tua avventura!
+              </h3>
+              <p className={style.pBtn}>
+              🚴‍♀️ Foggia ti aspetta: pedala, esplora e divertiti!
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={style.imageContainer}>
-        
+        <div className={style.imageContainer}>
+          <Image src="/hero.jpg" width={600} height={450} alt="hero-image" />
+        </div>
       </div>
     </>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>
+          © {new Date().getFullYear()} La mia azienda. Tutti i diritti
+          riservati.
+        </p>
+        <nav>
+          <ul className={styles.nav}>
+            <li>
+              <a href="/privacy">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/terms">Termini di Servizio</a>
+            </li>
+            <li>
+              <a href="/contact">Contatti</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
