844bd2ff4d1a760b056eccfa2893a6531bf8f413
Giorgio-g97 <giorgio.g97@gmail.com>
2024-10-17 17:37:25 +0200

    CSS form

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -2,9 +2,9 @@ export default function CreatePost(){
     return (
         <main className="text-center pt-32 px-10 flex flex-col gap-5">
             <h1 className="text-5xl font-bold">Crea un Post</h1>
-        <form >
-            <input type="text" placeholder="Inserisci un titolo.." />
-            <button className="border rounded-lg">Crea Post</button>
+        <form className="space-x-3">
+            <input className="p-1 border rounded-lg" type="text" placeholder="Inserisci un titolo.." />
+            <button className="bg-primary text-white p-1 rounded-lg">Crea Post</button>
         </form>
         </main>
     )
