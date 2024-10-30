14b9b504537e4f3da4c4c26f0f19ad5133dacd0f
Giorgio-g97 <giorgio.g97@gmail.com>
2024-10-30 22:25:07 +0100

    Autenticazione actions

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 /**
  * SERVER ACTIONS
@@ -10,6 +12,13 @@ import { revalidatePath } from "next/cache";
  * metodo 
  */
 export default async function createPost(formData: FormData) {
+    // auth check
+    const { isAuthenticated } = getKindeServerSession();
+
+    if(!(await isAuthenticated())) {
+        redirect("/api/auth/login");
+    }
+
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
 
